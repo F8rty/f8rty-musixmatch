9d58be525263c11b775570caf1e5f1508df17fe1
Luis Olivar <luis.olivar@nyu.edu>
2020-07-11 11:39:55 -0700

    Initial F8RTYMusicPlayer commit

---
+++ src/client/components/F8RTYMusicPlayer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../icons/logo.svg';
+
+function F8RTYMusicPlayer() {
+	return (
+		<div>
+			<div>This is the music player custom component.</div>
+			<img src={logo} className="App-logo" alt="logo" />
+		</div>
+	);
+}
+export default F8RTYMusicPlayer;
